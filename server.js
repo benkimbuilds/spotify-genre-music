c0acaa3febd29aeddea0e2c4da64969f5bab0f45
Locke <benkim96@gmail.com>
2026-03-04 12:22:55 -0600

    server bind

--- server.js
+++ server.js
@@ -147,6 +147,6 @@ app.get('*splat', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Genrexplore server running on port ${PORT}`);
 });
